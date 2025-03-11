9462b4630b855090508c3295484edc21efb508bd
PattanunW <6733191521@student.chula.ac.th>
2025-03-12 00:11:44 +0700

    commit 3

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,7 +3,7 @@ import styles from "./banner.module.css";
 const Banner = () => {
   return (
     <div className={styles.banner}>
-      <img src="/championswall.jpg" alt="Champions Wall" className={styles.image} />
+        <img/>
       <h1 className={styles.title}>where every event finds its venue</h1>
       <p className={styles.subtitle}>
         Anfield is the best football stadium to ever exist.
