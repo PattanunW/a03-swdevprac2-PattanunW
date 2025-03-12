95acae8d0d98959c3eda3b5986b37e2517186dd8
PattanunW <6733191521@student.chula.ac.th>
2025-03-12 16:01:30 +0700

    commit 7

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,7 +3,7 @@ import styles from "./banner.module.css";
 
 const Banner = () => {
   return (
-    <div className={styles.banner}>
+    <div className={styles.image}>
       <div className={styles.content}>
         <h1 className={styles.title}>where every event finds its venue</h1>
         <p className={styles.subtitle}>
